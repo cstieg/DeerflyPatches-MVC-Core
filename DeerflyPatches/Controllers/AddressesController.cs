8f45911c5352ff098ca1657b6c522c4044197b8b
cstieg <cstieg4899@yahoo.com>
2017-07-19 10:27:55 -0400

    Add Address model/view/controller

---
+++ DeerflyPatches/Controllers/AddressesController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DeerflyPatches.Models;
+
+namespace DeerflyPatches.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private readonly DeerflyPatchesContext _context;
+
+        public AddressesController(DeerflyPatchesContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Addresses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Address.ToListAsync());
+        }
+
+        // GET: Addresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Recipient,Address1,Address2,City,State,Zip,Country,Phone")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address.SingleOrDefaultAsync(m => m.ID == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Recipient,Address1,Address2,City,State,Zip,Country,Phone")] Address address)
+        {
+            if (id != address.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var address = await _context.Address.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Address.Remove(address);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Address.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ DeerflyPatches/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeerflyPatches.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        public Customer Owner { get; set; }
+        public string Recipient { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
