618ef6bb400f95f33e36df8fa9be7041479ee153
cstieg <cstieg4899@yahoo.com>
2017-07-19 10:41:21 -0400

    Migration for Orders and other new models

---
+++ DeerflyPatches/Migrations/20170719144014_Orders.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DeerflyPatches.Migrations
+{
+    public partial class Orders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address1 = table.Column<string>(nullable: true),
+                    Address2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    OwnerID = table.Column<int>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Recipient = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Zip = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Address_Customer_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Customer",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BillToID = table.Column<int>(nullable: true),
+                    DateOrdered = table.Column<DateTime>(nullable: false),
+                    PurchaserID = table.Column<int>(nullable: true),
+                    ShipToID = table.Column<int>(nullable: true),
+                    Shipping = table.Column<decimal>(nullable: false),
+                    Subtotal = table.Column<decimal>(nullable: false),
+                    Total = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Order_Address_BillToID",
+                        column: x => x.BillToID,
+                        principalTable: "Address",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Order_Customer_PurchaserID",
+                        column: x => x.PurchaserID,
+                        principalTable: "Customer",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Order_Address_ShipToID",
+                        column: x => x.ShipToID,
+                        principalTable: "Address",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CheckedOut = table.Column<bool>(nullable: false),
+                    ItemID = table.Column<int>(nullable: true),
+                    OrderID = table.Column<int>(nullable: true),
+                    PlacedInCart = table.Column<DateTime>(nullable: false),
+                    PurchaserID = table.Column<int>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Shipping = table.Column<decimal>(nullable: false),
+                    UnitPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Product_ItemID",
+                        column: x => x.ItemID,
+                        principalTable: "Product",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Order",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Customer_PurchaserID",
+                        column: x => x.PurchaserID,
+                        principalTable: "Customer",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_OwnerID",
+                table: "Address",
+                column: "OwnerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_BillToID",
+                table: "Order",
+                column: "BillToID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_PurchaserID",
+                table: "Order",
+                column: "PurchaserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ShipToID",
+                table: "Order",
+                column: "ShipToID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_ItemID",
+                table: "OrderDetail",
+                column: "ItemID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_OrderID",
+                table: "OrderDetail",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_PurchaserID",
+                table: "OrderDetail",
+                column: "PurchaserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
