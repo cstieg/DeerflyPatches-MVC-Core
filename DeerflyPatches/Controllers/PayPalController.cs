05881500d36eaa44ff4ebddee2e5d45b086ee772
cstieg <cstieg4899@yahoo.com>
2017-07-22 19:17:20 -0400

    Refactor order post to Paypal as member of PayPalApiClient

--- DeerflyPatches/Controllers/PayPalController.cs
+++ DeerflyPatches/Controllers/PayPalController.cs
@@ -38,86 +38,14 @@ namespace DeerflyPatches.Controllers
             ClientInfo paypalSecrets = paypalClient.GetClientSecrets(_env.ContentRootPath);
 
             // Get access token
-            AccessToken access_token = await paypalClient.GetAccessToken(paypalSecrets);
+            AccessToken accessToken = await paypalClient.GetAccessToken(paypalSecrets);
 
+            // Create JSON string with order information
+            string orderData = paypalClient.CreateOrder(shoppingCart);
 
-            // Create order object
-            object data = new
-            {
-                intent = "order",
-                payer = new {
-                    payment_method = "paypal"
-                },
-                transactions = new List<object>
-                {
-                    new
-                    {
-                        amount = new
-                        {
-                            currency = "USD",
-                            total = "10.00",
-                            details = new
-                            {
-                                shipping = "0.00",
-                                subtotal = "10.00",
-                                tax = "0.00"
-                            }
-                        },
-                        payee = new
-                        {
-                            email = "cstieg4899-facilitator@yahoo.com"
-                        },
-                        description = "Order from Detex, manufacturer of Deerfly Patches",
-                        item_list = new
-                        {
-                            items = new List<Object>
-                            {
-                                new
-                                {
-                                    name = "DFP",
-                                    quantity = "1",
-                                    price = "10.00",
-                                    sku = "1",
-                                    currency = "USD"
-                                }
-                            },
-                            shipping_address = new
-                            {
-                                recipient_name = "Christopher Stieg",
-                                line1 = "17852 Ten Mile Road",
-                                line2 = "",
-                                city = "LeRoy",
-                                country_code = "US",
-                                postal_code = "49655",
-                                phone = "2316800095",
-                                state = "MI"
-                            }
-                        }
-                    }
-                },
-                redirect_urls = new
-                {
-                    return_url = "http://localhost:50138/Home/ShoppingCart",
-                    cancel_url = "http://localhost:50138/Home/ShoppingCart"
-                }
-            };
-            string dataJSON = JsonConvert.SerializeObject(data);
-            // Create order with PayPal
-
-            string orderId = "";
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", access_token.AccessTokenString);
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://api.sandbox.paypal.com/v1/payments/payment");
-                request.Content = new StringContent(dataJSON, Encoding.UTF8, "application/json");
-                var response = await client.SendAsync(request);
-                var result = response.Content.ReadAsStringAsync().Result;
-                orderId = new JsonDeserializer(result).GetString("id");
-
-            }
-
-
-            return orderId;
+            // Post order to PayPal API and return order ID to front end
+            string orderID = await paypalClient.PostOrder(orderData, accessToken.AccessTokenString);
+            return orderID;
         }
 
 
--- DeerflyPatches/Modules/PayPal/PayPalApiClient.cs
+++ DeerflyPatches/Modules/PayPal/PayPalApiClient.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using DeerflyPatches.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,14 +41,90 @@ namespace DeerflyPatches.Modules.PayPal
                 var response = await client.SendAsync(request);
                 var result = response.Content.ReadAsStringAsync().Result;
                 return JsonConvert.DeserializeObject<AccessToken>(result);
-                //return new JsonDeserializer(result).GetString("access_token");
-
-
             }
-
         }
 
+        public string CreateOrder(ShoppingCart shoppingCart)
+        {
+            object data = new
+            {
+                intent = "order",
+                payer = new
+                {
+                    payment_method = "paypal"
+                },
+                transactions = new List<object>
+                {
+                    new
+                    {
+                        amount = new
+                        {
+                            currency = "USD",
+                            total = "10.00",
+                            details = new
+                            {
+                                shipping = "0.00",
+                                subtotal = "10.00",
+                                tax = "0.00"
+                            }
+                        },
+                        payee = new
+                        {
+                            email = "cstieg4899-facilitator@yahoo.com"
+                        },
+                        description = "Order from Detex, manufacturer of Deerfly Patches",
+                        item_list = new
+                        {
+                            items = new List<Object>
+                            {
+                                new
+                                {
+                                    name = "DFP",
+                                    quantity = "1",
+                                    price = "10.00",
+                                    sku = "1",
+                                    currency = "USD"
+                                }
+                            },
+                            shipping_address = new
+                            {
+                                recipient_name = "Christopher Stieg",
+                                line1 = "17852 Ten Mile Road",
+                                line2 = "",
+                                city = "LeRoy",
+                                country_code = "US",
+                                postal_code = "49655",
+                                phone = "2316800095",
+                                state = "MI"
+                            }
+                        }
+                    }
+                },
+                redirect_urls = new
+                {
+                    return_url = "http://localhost:50138/Home/ShoppingCart",
+                    cancel_url = "http://localhost:50138/Home/ShoppingCart"
+                }
+            };
+            string dataJSON = JsonConvert.SerializeObject(data);
+            return dataJSON;
+        }
 
+        public async Task<string> PostOrder(string data, string accessToken)
+        {
+
+            string orderId = "";
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://api.sandbox.paypal.com/v1/payments/payment");
+                request.Content = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = await client.SendAsync(request);
+                var result = response.Content.ReadAsStringAsync().Result;
+                orderId = new JsonDeserializer(result).GetString("id");
+            }
+            return orderId;
+        }
 
     }
 }
